3f7dbde3f24e58070abd1602d4561e3af3d33770
Александр <achernov@aplana.com>
2019-04-07 18:26:09 +0300

    Initial Commit

---
+++ HWSberPingPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWSberPingPong
+//
+//  Created by Александр on 07/04/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWSberPingPong/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HWSberPingPong
+//
+//  Created by Александр on 07/04/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
